eb2e3ed209345204835655b2c56306197d063ecc
jimmy16 <waiwai@bi.ciao.jp>
2025-08-07 22:50:42 +0900

    20250807_kuroko設定

---
+++ app/pages/takarazuka/skystage/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>News list</p>
+    <div v-for="n in response.list" :key="n.slug">
+      <nuxt-link :to="`/skystage/${n.topics_id}`">
+        {{ n.ymd }} {{ n.subject }}
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const config = useRuntimeConfig();
+const { data: response } = await useFetch('/rcms-api/3/skystage',{
+  baseURL:config.public.apiBase,
+  credentials: 'include',
+});
+</script>
